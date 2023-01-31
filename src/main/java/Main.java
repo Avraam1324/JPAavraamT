c5ce69213283925c4db9d4373e9463747537bb72
trpyy <trpttnbrh@gmail.com>
2023-01-31 20:30:05 +0200

    משימה JPA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,33 +8,43 @@ public class Main {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
-        try
-        {
+        try {
             transaction.begin();
 
+            //הדפסת איבר
             Query query = entityManager.createNamedQuery("SELECT");
             List<TryTable> list = query.getResultList();
-            for( TryTable user: list ){
+            for (TryTable user : list) {
                 System.out.println(user.toString());
             }
 
-
-            entityManager.persist(insert("sangoko"));
-
-            transaction.commit();
-        }
-        finally {
-            if (transaction.isActive()) {
-                transaction.rollback();
+            // עדכון
+            for (TryTable user : list) {
+                if (user.getTryColoms().equals("worked")) {
+                    user.setTryColoms("yhkka");
+                    entityManager.persist(user);
+                }
             }
-            entityManager.close();
-            entityManagerFactory.close();
+
+//                entityManager.persist(insert("sangoko"));
+
+                transaction.commit();
+            }
+        finally{
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+                entityManager.close();
+                entityManagerFactory.close();
+            }
         }
-    }
-    public static TryTable insert(String name){
-        TryTable tryTable = new TryTable();
-        tryTable.setTryColoms(name);
-    return tryTable;
+
+        //הכנסת איבר
+        public static TryTable insert (String name){
+            TryTable tryTable = new TryTable();
+            tryTable.setTryColoms(name);
+            return tryTable;
+        }
+
     }
 
-}
